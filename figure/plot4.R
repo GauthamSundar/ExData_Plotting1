642e765e1d924a1c7d16e9905d341a2d10ff55ee
GauthamSundar <gautham.sundar@outlook.com>
2015-07-12 23:41:42 +0530

    Modified plot 4 and R file 4

--- figure/plot4.R
+++ figure/plot4.R
@@ -3,7 +3,7 @@ x <- d[d[,"Date"]%in% c("1/2/2007","2/2/2007"),]
 library(lubridate)
 s <- paste(x[,1],x[,2]) # Combining Date and time
 w <- dmy_hms(s)
-png("plot5.png", width=480, height=480 )
+png("plot5.png", width=600, height=600 )
 par(mfrow = c(2,2))
 
 
@@ -25,7 +25,7 @@ m3 <- as.numeric(x[,9])
 plot(w,m1,type = "l", xlab = "", ylab = "Energy sub-metering")
 lines(w,m2,type = "l", col = "Red")
 lines(w,m3,type = "l", col = "Blue")
-legend("topright",, legend= c("sub-metering_1","sub-metering_2","sub-metering_3"), lty = c(1,1), col=c("Black","Red","Blue"), cex = 0.6)
+legend("topright",, legend= c("sub-metering_1","sub-metering_2","sub-metering_3"), lty = c(1,1), col=c("Black","Red","Blue"), cex = 0.90)
 
 #Global reactive power
 
